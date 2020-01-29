28a859d516143b71ad6406aed86340b10019d1a5
“Orla <2391647s@student.gla.ac.uk>
2020-01-29 15:03:23 +0000

    chapter 6 update

--- rango/views.py
+++ rango/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rango.models import Category
 from django.http import HttpResponse
+from rango.models import Page
 
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
+    category_list = Category.objects.order_by('-likes')[:5]
+
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+    
     return render(request, 'rango/index.html', context=context_dict)
     
 
@@ -17,3 +17,18 @@ def about(request):
     context_dict = {'boldmessage': 'This tutorial has been put together by Orla Sonvico'}
     return render(request, 'rango/about.html', context=context_dict)
     #return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+        
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    return render(request, 'rango/category.html', context=context_dict)
