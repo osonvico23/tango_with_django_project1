dbc90f0ca5ba1f2741189c23a95e6069bcdf1924
“Orla <2391647s@student.gla.ac.uk>
2020-01-30 12:35:14 +0000

    chap 7 update

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rango.models import Category
 from django.http import HttpResponse
 from rango.models import Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -41,4 +43,17 @@ def show_page(request, urls):
         context_dict['pages'] = None
 
     return render(request, 'rango/index.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form':form})
+    
     
