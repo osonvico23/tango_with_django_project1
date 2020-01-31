8b9b30aca9d132e6cc7e473c9f418a3ac61c89b3
“Orla <2391647s@student.gla.ac.uk>
2020-01-31 14:05:01 +0000

    chap 7 complete

--- rango/forms.py
+++ rango/forms.py
@@ -24,7 +24,7 @@ class PageForm(forms.ModelForm):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
 
-        if url and not url.starstwith('http://'):
+        if url and not url.startswith('http://'):
             url = f'http://{url}'
             cleaned_data['url'] = url
 
